2390f891f3af7f1c70e83aa3b720dfc1344a1efe
Albert Darren <xiweideng@stu.xmu.edu.cn>
2024-06-03 13:30:09 +0800

    finshed the training and testing of theclassification task of imagenet-1k

--- dist_test.sh
+++ dist_test.sh
@@ -2,17 +2,16 @@
 
 MODEL="efficientformer_l1"
 nGPUs=2
-CUDA_VISIBLE_DEVICES="0,1"
+export CUDA_VISIBLE_DEVICES="0,1"
 OUTPUT_DIR="efficientformer_test"
-DATA_PATH="/path/to/imagenet"
+DATASET="IMNET"
+DATA_PATH="/data1/datasets/imagenet"
 CHECKPOINT=$OUTPUT_DIR/checkpoint.pth
 
-nohup CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES \
-python -m torch.distributed.launch \
---nproc_per_node=$nGPUs \
---use_env main.py \
+nohup torchrun --nproc_per_node=$nGPUs main.py \
 --model $MODEL \
 --resume $CHECKPOINT \
 --eval \
+--data-set $DATASET \
 --data-path $DATA_PATH \
 --output_dir $OUTPUT_DIR > test.txt 2>&1 &
--- dist_train.sh
+++ dist_train.sh
@@ -1,31 +1,17 @@
 #!/usr/bin/env bash
 
 MODEL="efficientformer_l1"
-nGPUs=2
-CUDA_VISIBLE_DEVICES="0,1"
-BATCH_SIZE=64
-EPOCHS=5
-DATA_PATH="/path/to/imagenet"
-OUTPUT_DIR="efficientformer_l1_300d"
-RESUME=""
-EVAL=""
-FINETUNE=""
-DISTILLATION_TYPE=""
-DISTILLATION_ALPHA=""
-DISTILLATION_TAU=""
-
-nohup CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES \
-python -m torch.distributed.launch \
---nproc_per_node=$nGPUs \
---use_env main.py \
+DATASET="IMNET"
+nGPUs=4
+export CUDA_VISIBLE_DEVICES="0,1,2,7"
+BATCH_SIZE=128
+EPOCHS=2
+DATA_PATH="/data1/datasets/imagenet"
+OUTPUT_DIR="efficientformer_l1_${EPOCHS}d"
+nohup torchrun --nproc_per_node=$nGPUs main.py \
 --model $MODEL \
+--data-set $DATASET \
 --data-path $DATA_PATH \
 --output_dir $OUTPUT_DIR \
---resume $RESUME \
---eval $EVAL \
---finetune $FINETUNE \
---distillation_type $DISTILLATION_TYPE \
---distillation_alpha $DISTILLATION_ALPHA \
---distillation_tau $DISTILLATION_TAU \
 --batch-size $BATCH_SIZE \
---epochs $EPOCHS > train.txt 2>&1 &
+--epochs $EPOCHS > train_${EPOCHS}.txt 2>&1 &
